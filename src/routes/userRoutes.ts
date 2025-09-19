ffe5668e297fca275ecd2b885aad22c4e3eac593
alvin-mob <alwinedgar70@gmail.com>
2025-09-20 03:28:53 +0530

    fix(user): exception handling error codes updated

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth";
 import { errorHandler } from "../errorHandler";
-import { addAddress, deleteAddress, getAddresses, updateAddress } from "../controllers/addressController";
+import { addAddress, deleteAddress, getAddresses, updateAddress, updateUser } from "../controllers/userController";
 
 const userRoutes : Router =  Router();
 
@@ -9,5 +9,6 @@ userRoutes.post('/address',[authMiddleware], errorHandler(addAddress));
 userRoutes.get('/address', [authMiddleware], errorHandler(getAddresses));
 userRoutes.put('/address/:id', [authMiddleware], errorHandler(updateAddress));
 userRoutes.delete('/address/:id', [authMiddleware], errorHandler(deleteAddress));
+userRoutes.put('/',[authMiddleware], errorHandler(updateUser));
 
 export default userRoutes;
